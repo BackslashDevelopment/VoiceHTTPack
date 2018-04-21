c6d75add034abf337ab468a75bf342e2889b06af
Robin Lindén <dev@robinlinden.eu>
2018-04-22 00:42:20 +0200

    Add project files and stubs of the functions required

---
+++ VoiceHTTPAck/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("VoiceHTTPAck")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VoiceHTTPAck")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("ae0756cb-ec41-4021-848a-a7a3f1e1a8a5")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
---
+++ VoiceHTTPAck/VoiceHTTPAck.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+
+namespace VAPlugin
+{
+    public class VoiceHTTPAck
+    {
+        // Displayed in dropdowns and log files.
+        public static string VA_DisplayName()
+        {
+            return "VoiceHTTPAck";
+        }
+
+        // Extended display information. Probably name + description + copyright year.
+        public static string VA_DisplayInfo()
+        {
+            return "VoiceHTTPAck";
+        }
+
+        // Used by VoiceAttack to identify this plugin.
+        public static Guid VA_Id()
+        {
+            return new Guid("{ae0756cb-ec41-4021-848a-a7a3f1e1a8a5}");
+        }
+
+        // Called when a "stop all commands" action is called.
+        public static void VA_StopCommand()
+        {
+        }
+
+        // Called asynchronously once on VoiceAttack load.
+        public static void VA_Init1(dynamic vaProxy)
+        {
+        }
+
+        // Called when VoiceAttack is shutting down.
+        public static void VA_Exit1(dynamic vaProxy)
+        {
+        }
+
+        // Called when a 'Execute External Plugin Function' action is called. Do all your work in this function.
+        public static void VA_Invoke1(dynamic vaProxy)
+        {
+        }
+    }
+}
